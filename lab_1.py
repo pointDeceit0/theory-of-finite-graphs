8cf13ce1261727477ab9925f998a61636a799824
pointDeceit0 <fedor.korolev.87@mail.ru>
2022-10-08 00:30:27 +0300

    Lab_1.py

---
+++ lab_1.py
@@ -0,0 +1,36 @@
+from algorithms.max_tree import max_subgraph
+from print_graph import print_graph
+
+
+def main():
+    n = int(input('Введите число вершин: '))
+
+    gr = []
+    while (inp := input("Введите ребро графа в формате [V1 V2 вес] через пробелы или введите 'q', если больше нет ребер: ")) != 'q':
+        inp = inp.strip().split()
+        inp[2] = float(inp[2])
+        gr.append(inp)
+
+    print_graph(gr)
+    gr, weight = max_subgraph(gr, n)
+    print_graph(gr)
+    print(weight)
+
+
+def test():
+    with open('data.txt', 'r') as file:
+        gr = []
+        n = int(file.readline().strip())
+
+        while (inp := file.readline()) != 'q':
+            inp = inp.strip().split()
+            inp[2] = float(inp[2])
+            gr.append(inp)
+
+    print_graph(gr)
+    gr = max_subgraph(gr, n)
+    print_graph(gr)
+
+
+if __name__ == "__main__":
+    main()
