c6ce6eae9c7a688ca3ce8250ce11277220bf1767
pointDeceit0 <fedor.korolev.87@mail.ru>
2022-10-07 23:05:33 +0300

    Fix bag

--- algorithms/max_tree.py
+++ algorithms/max_tree.py
@@ -1,13 +1,23 @@
-def max_subgraph(graph: list[list[str, str, int]]) -> list[tuple[str, str, float]]:
+class DisconnectedGraph(Exception):
+    pass
+
+
+def max_subgraph(graph: list[list[str, str, int]], n: int) -> list[tuple[str, str, float]]:
     "Prim's algorithm"
 
     e = sorted(graph, reverse=True, key=lambda x: x[2])
     
     ans, bouquet = [], set()
-    for _, v in enumerate(e):
+    for i, v in enumerate(e):
         # check for a cycle
         if not (v[0] in bouquet and v[1] in bouquet): 
-            ans.append(tuple(v))
+            for j in range(i + 1, len(e)):
+                if v[0] in e[j] or v[1] in e[j]:
+                    ans.append(tuple(v))
+                    break
+            else:
+                raise DisconnectedGraph
+
             bouquet.add(v[0])
             bouquet.add(v[1])
 
@@ -18,8 +28,8 @@ def main():
 
     
     gr = []
-    for _ in range(n):
-        inp = input("Input edge of graph in format [vertex_1 vertex2 weight] by space: ").strip().split()
+    while (inp := input("Input edge of graph in format [vertex_1 vertex2 weight] by space OR input q if there's no edges : ")) != 'q':
+        inp = inp.strip().split()
         inp[2] = float(inp[2])
         gr.append(inp)
 
